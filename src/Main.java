0b09fda9403162b0357b90fa935e70aa11fabdee
zhaobingtao <zhaobingtao@zhiketong.cn>
2018-09-13 15:07:55 +0800

    用Idea第一次提交

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("我是来捣乱的！");
+        System.out.println("add class user!");
     }
 }
