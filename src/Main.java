14b308eb10e2093c0d4618721c80e300d0beb65a
zhaobingtao <zhaobingtao@zhiketong.cn>
2018-09-13 17:03:06 +0800

    哈哈哈扯淡

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("是张三派我来捣乱的");
+        System.out.println("add Class User");
     }
 }
