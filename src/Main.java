e5b6c1136babdf8ca22c40752ed0c97af3789526
13718722321 <275851127@qq.com>
2018-09-13 16:35:06 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("我是来捣乱的！");
-        System.out.println("add class user!");
     }
 }
